72314ac174c4e1825178a327f32279fbddf38355
devasher <hlxstc680@gmail.com>
2025-11-01 00:02:47 +0800

    fix(vite): add nodePolyfills to use nodejs Buffer for vite project

--- examples/fhevm-vite-vue/vite.config.ts
+++ examples/fhevm-vite-vue/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      // To add only specific polyfills, add them here
+      // If no option is passed, all polyfills are included
+      include: ['buffer'],
+      globals: {
+        Buffer: true,
+      },
+    }),
+  ],
+
+  optimizeDeps: {
+    exclude: ['@zama-fhe/relayer-sdk'],
+  },
 })
